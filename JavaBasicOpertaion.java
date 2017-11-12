a3ab1c88aa3e87964bb54b14ef1581c2c5c5f389
jilongbai <jilongbai@126.com>
2017-11-13 01:01:39 +0800

    addition operation for Java

---
+++ JavaBasicOpertaion.java
@@ -0,0 +1,21 @@
+package com.magadistudio.javabasicoperation;
+
+public class JavaBasicOpertaion 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	
+	    //explanation of the function of "println"
+		//println里面只有字符串，则其他项也全部按照字符串处理
+		System.out.println('a' + 1);
+		System.out.println(" " + 'a' + 1);
+		System.out.println("123" + 234 + 1);
+		System.out.println("bhf" + 234 + 1);
+		System.out.println('b' + 'a' + 1);
+		
+	    //addtional operator
+	}
+
+}
